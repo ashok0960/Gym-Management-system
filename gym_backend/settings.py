e0c3df8ad2d1f9a9016d81ac0783f9b9c1ec426f
ashok0960 <ashokkumarkarki5@gmail.com>
2026-07-04 16:31:00 +0545

    Database Change

--- gym_backend/settings.py
+++ gym_backend/settings.py
@@ -107,9 +107,17 @@ WSGI_APPLICATION = 'gym_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GymDB',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
